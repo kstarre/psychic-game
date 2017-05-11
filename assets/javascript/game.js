58753ddd6bb1b3cb963a22de019d7a653cb15e6d
Katie Starrett <starrett.k.e@gmail.com>
2017-05-11 17:39:42 -0400

    add final functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,12 +4,12 @@ var guessesLeft = 9;
 var lettersGuess = "";
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+
 window.onload = function(event) {
 	document.getElementById("wins").innerHTML = wins;
 	document.getElementById("losses").innerHTML = losses;
 	document.getElementById("guessLeft").innerHTML = guessesLeft;
-	document.getElementById("test").innerHTML = compGuess;
-}
+};
 document.onkeyup = function(event) {
 	var userChoice = event.key;
 	lettersGuess = lettersGuess + userChoice + " ";
@@ -17,44 +17,23 @@ document.onkeyup = function(event) {
 	if (userChoice === compGuess) {
 		wins++;
 		lettersGuess = "";
+		guessesLeft = 9;
+		compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 	}
 	else {
-		losses++;
+		guessesLeft--;
+		if (guessesLeft === 0) {
+			losses++;
+			lettersGuess = "";
+			guessesLeft = 9;
+			compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+		}
 	}
 
 	document.getElementById("lettersGuess").innerHTML = lettersGuess;
 	document.getElementById("wins").innerHTML = wins;
 	document.getElementById("losses").innerHTML = losses;
-
-/*			if (userChoice === compGuess) {
-				wins++;
-				document.getElementById("wins").innerHTML = wins;
-			}
-			else {
-				guessesLeft--;
-				document.getElementById("lettersGuess").innerHTML = lettersGuess;
-				document.getElementById("numGuess").innerHTML = guessesLeft;
-			}*/
-/*			losses++;
-			document.getElementById("losses").innerHTML = losses;
-		}*/
+	document.getElementById("guessLeft").innerHTML = guessesLeft;
 
 
-/*	loop1:
-	for (var i = 0; i < 9 ; i++) {
-		document.getElementById("numGuess").innerHTML = guessesLeft;
-		if (userChoice === compGuess) {
-			wins++;
-			document.getElementById("wins").innerHTML = wins;
-			break loop1;
-		}
-		else {
-			guessesLeft--;
-			document.getElementById("lettersGuess").innerHTML = lettersGuess;
-		}
-	}
-	if (guessesLeft === 0) {
-		losses++;
-		document.getElementById("losses").innerHTML = losses;
-	};*/
 };
