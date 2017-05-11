3e4502b1bc4094a9cc8e5ea7680ceb6450e86ca4
Katie Starrett <starrett.k.e@gmail.com>
2017-05-11 16:07:45 -0400

    write guessesLeft functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,11 @@ var lettersGuess = "";
 var compGuess = "";
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+window.onload = function(event) {
+	document.getElementById("wins").innerHTML = wins;
+	document.getElementById("losses").innerHTML = losses;
+	document.getElementById("guessLeft").innerHTML = guessesLeft;
+}
 document.onkeyup = function(event) {
 	var userChoice = event.key;
 	compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
