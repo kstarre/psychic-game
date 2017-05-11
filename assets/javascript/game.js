ca722e98e52f91afbb4c27d49e468a90cb866d38
Katie Starrett <starrett.k.e@gmail.com>
2017-05-11 17:17:11 -0400

    add lettersGuess functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,21 +2,21 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var lettersGuess = "";
-var compGuess = "";
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 window.onload = function(event) {
 	document.getElementById("wins").innerHTML = wins;
 	document.getElementById("losses").innerHTML = losses;
 	document.getElementById("guessLeft").innerHTML = guessesLeft;
+	document.getElementById("test").innerHTML = compGuess;
 }
 document.onkeyup = function(event) {
 	var userChoice = event.key;
-	compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 	lettersGuess = lettersGuess + userChoice + " ";
 
 	if (userChoice === compGuess) {
 		wins++;
+		lettersGuess = "";
 	}
 	else {
 		losses++;
